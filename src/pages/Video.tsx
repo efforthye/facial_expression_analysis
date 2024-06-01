c539e079f6c58e75f3bd38a8e980e9131ea82637
cxt-heizel <153474603+cxt-heizel@users.noreply.github.com>
2024-06-01 12:33:03 +0900

    비디오 페이지 퍼블리싱

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState } from 'react';
 import * as faceapi from 'face-api.js';
 import '@tensorflow/tfjs-backend-webgl';
 import styled from 'styled-components';
 import axios from 'axios';
+
+import Button from '@mui/joy/Button';
 
 const expressionMapping: any = {
   neutral: '무표정인 것 같아 보여요!',
@@ -14,11 +16,33 @@ const expressionMapping: any = {
   surprised: '좀 놀라 보여요!'
 };
 
+
+interface VideoComponentProps {
+  countdown: number;
+}
+
 const VideoComponent: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const expressionsRef = useRef<HTMLDivElement>(null);
   const speechRef = useRef<SpeechSynthesisUtterance | null>(null);
+
+  const [countdown, setCountdown] = useState(20);
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown <= 1) {
+          clearInterval(timer);
+          return 0;
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   useEffect(() => {
     const loadModels = async () => {
@@ -138,11 +162,46 @@ const VideoComponent: React.FC = () => {
 
   return (
     <Container>
-      <VideoContainer>
-        <StyledVideo ref={videoRef} autoPlay muted />
-        <StyledCanvas ref={canvasRef} />
-        <ExpressionDiv ref={expressionsRef} />
-      </VideoContainer>
+      <Header>
+        <div className="wrapper">
+          <a>X</a>
+          <div>
+            <div className="badge">CHAPTER.2</div>
+            <div className="bar"></div>
+            <p>표정 짓기</p>
+          </div>
+        </div>
+      </Header>
+      <BodyWrapper>
+        <VideoContainer>
+          <StyledVideo ref={videoRef} autoPlay muted />
+          <StyledCanvas ref={canvasRef} />
+          <ExpressionWrapper>
+            <ExpressionDiv ref={expressionsRef} />
+          </ExpressionWrapper>
+        </VideoContainer>
+        <CountdownContainer>
+          <p>{countdown}</p>
+        </CountdownContainer>
+      </BodyWrapper>
+      <FooterWrapper>
+        <Button
+           color="primary"
+           onClick={function(){}}
+           size="lg"
+           variant="soft"
+        >건너뛰기</Button> 
+        <div className="question">
+          <span>행복한 표정</span>을 지어볼까요? (제한시간 20초)
+        </div>
+        <Button
+          color="primary"
+          onClick={function(){}}
+          size="lg"
+          variant="solid"
+        >다음으로</Button> 
+      </FooterWrapper>
+      
     </Container>
   );
 };
@@ -156,10 +215,66 @@ const Container = styled.div`
   height: 100vh;
 `;
 
+const Header = styled.div`
+  display: block;
+  width: 100%;
+  height: 68px;
+  position: fixed;
+  top: 0;
+  > .wrapper{
+    display: grid;
+    align-items: center;
+    grid-template-columns: 24px 1fr;
+    gap: 19px;
+    height: 100%;
+    padding: 0 30px;
+    > div{
+      display: flex;
+      align-items: center;
+      height: 100%;
+      gap: 10px;
+      .badge{
+        padding: 8px 11px;
+        border-radius: 32px;
+        border: 2px solid #4F5256;
+        background: #272A2C;
+        color: #EDF0F2;
+        text-align: center;
+        font-size: 10px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 13px; /* 130% */
+      }
+      .bar{
+        width: 1px;
+        height: 25px;
+        background: #D9D9D9;
+      }
+      > p{
+        color: #4F5256;
+        font-size: 15px;
+        font-weight: 700;
+        line-height: normal;
+      }
+    }
+  }
+  
+`;
+
+const BodyWrapper = styled.div`
+  width: 855px;
+  height: 402px;
+  border-radius: 27px;
+  overflow: hidden;
+  position: relative;
+`
+
 const VideoContainer = styled.div`
   position: relative;
-  width: 720px;
-  height: 560px;
+  width: 101%;
+  height: 161%;
+  margin-top: -60px;
+  box-shadow: 1px 1px 80px #7d80853b;
 `;
 
 const StyledVideo = styled.video`
@@ -180,13 +295,70 @@ const StyledCanvas = styled.canvas`
   height: 100%;
 `;
 
+const ExpressionWrapper = styled.div`
+  position: fixed;
+  width: 400px;
+  height: 44px;
+  padding: 10px 20px;
+  bottom: 158px;
+  left: calc( 50% - 200px );
+  box-sizing: border-box;
+  border-radius: 36px;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0px 5px 20px 0px var(--Shadows-300, rgba(0, 0, 0, 0.15));
+  backdrop-filter: blur(40px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  `;
+
 const ExpressionDiv = styled.div`
-  position: absolute;
-  top: -35px;
-  left: 10px;
+  text-align: center;
   color: white;
-  background-color: rgb(255, 186, 10);
-  padding: 5px;
-  border-radius: 5px;
-  font-size: large;
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 120%; /* 18px */
+  
+`;
+
+const CountdownContainer = styled.div`
+  position: absolute;
+  border-radius: 50%;
+  border: 1px solid #fff;
+  right: 16px;
+  bottom: 14px;
+  width: 48px;
+  height: 48px;
+  text-align: center;
+  line-height: 18px;
+  background: #00000014;
+  p{
+    color: #fff;
+    font-size: 15px;
+    font-weight: 500;
+  }
+`
+
+const FooterWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  padding: 27px 42px;
+  box-sizing: border-box;
+  > .question{
+    border-radius: 60px;
+    background: #243686;
+    color: #fff;
+    display: flex;
+    padding: 10px 20px;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    span{
+      font-weight: 800;
+    }
+  }
 `;
